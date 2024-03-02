318563ca7a6aaa0ff10b88e86f12a4bd2be5e571
RobotRamen <85594912+RobotRamen@users.noreply.github.com>
2024-03-02 10:35:19 -0500

    reduced pixeller resolution, increased play space to 1920x1080

--- pixeller.gd
+++ pixeller.gd
@@ -2,16 +2,16 @@ extends Node2D
 
 @onready var pixel = preload("res://pixel.tscn")
 
-var pixel_density = 6
+var pixel_density = 10
 
 # The thread will start here.
 func _ready():
-	for i in 1740/pixel_density:
-		for j in 900/pixel_density:
+	for i in 1820/pixel_density:
+		for j in 980/pixel_density:
 			if (i+j)%2:
 				var new_pixel : Node2D = pixel.instantiate()
 				add_child(new_pixel)
-				new_pixel.position = Vector2(i*pixel_density+pixel_density/2.0,j*pixel_density+pixel_density/2.0)
+				new_pixel.position = Vector2(i*pixel_density+pixel_density/2.0 + 50,j*pixel_density+pixel_density/2.0 + 50)
 				for side_sprite : CanvasItem in new_pixel.get_node("side_sprites").get_children():
 						side_sprite.position += Vector2(randf_range(-10., +10.),randf_range(-10., +10.))
 						side_sprite.scale *= randf_range(0.4, 1.8)
